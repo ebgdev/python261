b1456e130f677ef575ddc2e86d67a628e060e135
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2026-03-07 21:17:44 +0300

    Ders015: Map(), Filter() with lamda

--- ders015/odev15.py
+++ ders015/odev15.py
@@ -1,15 +1,10 @@
 import math
-
 new_array = [i for i in range(1,101)]
 
-def is_prime(number):
-    if number <= 1:
-        return False
-    for i in range(2,(number//2)+1):
-        if number % i == 0:
-            return False
-    return True
+# Elimizde 1 den 100 kadar bir liste olsun
+# filter ile nasil sadece asal sayilari secebilirim ?
 
-
-primes = list(filter(lambda x: is_prime(x),new_array))
-print(primes)
+# ipucu 
+# filter(function,iterable)
+# bize bir fonksiyon gerek, liste belli
+# bu fonksiyonu biz olusturabilir miyiz ?
