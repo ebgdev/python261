b5cd0d7399ad8a47a4788e95dc6c46fc55799f83
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2026-03-19 22:21:29 +0300

    Ders017: Sets part1 Dicts part1

--- ders017/odev16ans.py
+++ ders017/odev16ans.py
@@ -15,3 +15,20 @@ print(f"{odds=}")
 
 
 # --------------------------------
+
+matrix1 = [[1, 2, 12],[4, 5, 16],[3, 8, 9]]
+
+
+matrix2 = [[11, 8, 7],[6, 50, 4],[3, 2, 13]]
+
+# ipucu: Once bos bir 3x3 result matrix'i acariz
+# verileri guncelleyip buraya yazmamiz gerek.
+result = [[0,0,0],[0,0,0],[0,0,0]]
+
+
+for i in range(len(matrix1)): # i: 0,1,2, matrix1[i] = 1,2,12
+    for j in range(len(matrix1[i])): # j: 0,1,2 - 0,1,2 - 0,1,2
+        result[i][j] =  matrix1[i][j] + matrix2[i][j]
+
+
+print(result)
