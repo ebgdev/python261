c9b330ae82a7e06faf5e69aa2c6b17ef8cab8b93
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2026-04-09 01:56:25 +0300

    Ders021: Modules part1

--- ders021/modules/file2.py
+++ ders021/modules/file2.py
@@ -1,14 +1,19 @@
-from file1 import multi_10
+# # from file1 import multi_10
+# import file1
+# def add_ten(num):
+#     return num + 10
 
-def add_ten(num):
-    return num + 10
-
-# main : file2
-# Eger burasi ana akis ise
+# # main : file2
+# # Eger burasi ana akis ise
 
 
-print("file2.py: ",__name__)
+# print("file2.py: ",__name__)
 
-if __name__ == "__main__":
-    print(add_ten(3))
-    print(multi_10(5))
+# if __name__ == "__main__":
+#     print(add_ten(3))
+#     # print(multi_10(5))
+#     print(file1.multi_10(5))
+
+
+import math
+print(math.e)
